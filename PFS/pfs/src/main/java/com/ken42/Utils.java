8eba7ffd588333e5928e554a02bbcbb4d16266d7
varunken42 <111035240+varunken42@users.noreply.github.com>
2022-12-13 10:26:10 +0530

    added sleep for sbmp login

--- PFS/pfs/src/main/java/com/ken42/Utils.java
+++ PFS/pfs/src/main/java/com/ken42/Utils.java
@@ -216,11 +216,13 @@ public class Utils {
 						Utils.callSendkeys(driver, ActionXpath.username, studentuname, time, log);
 						Utils.callSendkeys(driver, ActionXpath.password, studentpassword, time, log);
 						Utils.clickXpath(driver, ActionXpath.singnin, time, "Verify", log);
+						Utils.bigSleepBetweenClicks(2);
 
 					} else if (Email.contains("faculty")) {
 						Utils.callSendkeys(driver, ActionXpath.username, facultyuname, time, log);
 						Utils.callSendkeys(driver, ActionXpath.password, facultypassword, time, log);
 						Utils.clickXpath(driver, ActionXpath.singnin, time, "Verify", log);
+						Utils.bigSleepBetweenClicks(2);
 
 					}
 				}
